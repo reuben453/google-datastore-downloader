2db9c3dcd412e5af0a860087a387bda6ab108e12
Reuben Pereira <reuben453@gmail.com>
2018-08-19 21:38:00 +0530

    The ruby datastore client limits results to 300 per batch. Ensure that all results are downloaded if there are more than 300 available by running the query again.

--- datastore_downloader.rb
+++ datastore_downloader.rb
@@ -19,19 +19,59 @@ rescue
 end
 
 column_names = raw_pkts.first.properties.to_h.keys
-s=CSV.generate do |csv|
-  csv << column_names
-  raw_pkts.each_with_index do |x, index|
-    temp_hash = x.properties.to_h
-    if temp_hash.keys.sort != column_names.sort
-      raise "All values do not have the same columns. Columns of 1st value: #{column_names.sort}, Columns of #{index+1} value: #{temp_hash.keys.sort}"
+
+first_loop = true
+
+catch :foreverloop do
+  while true do
+    s=CSV.generate do |csv|
+
+      # Add the column headers to the csv if this is the first loop
+      if first_loop
+        csv << column_names
+        first_loop = false
+      end
+
+      raw_pkts.each_with_index do |x, index|
+        temp_hash = x.properties.to_h
+
+        # Make sure the columns that this row has is the same as the columns of the first row
+        if temp_hash.keys.sort != column_names.sort
+          raise "All values do not have the same columns. Columns of 1st value: #{column_names.sort}, Columns of #{index+1} value: #{temp_hash.keys.sort}"
+        end
+
+        # Add each column of this row in the same order as the csv header row
+        values = []
+        column_names.each do |property|
+          values << temp_hash[property]
+        end
+
+        csv << values
+      end
+
     end
-    values = []
-    column_names.each do |property|
-      values << temp_hash[property]
+
+    # Print the generated csv
+    puts s
+
+    # If there was a limit set in the query, reduce the limit and check if we need to continue.
+    if raw_pkts.query.limit.value > 0
+      # Reduce the limit by the number of rows we got in the last run
+      raw_pkts.query.limit.value -= raw_pkts.length
+
+      # Exit the forever loop if we have the number of desired rows.
+      if raw_pkts.query.limit.value <= 0
+        throw :foreverloop
+      end
     end
-    csv << values
+
+    # Check if more results are available.
+    if raw_pkts.next?
+      # Get the next batch of results.
+      raw_pkts = raw_pkts.next
+    else
+      throw :foreverloop
+    end
+
   end
 end
-
-puts s
